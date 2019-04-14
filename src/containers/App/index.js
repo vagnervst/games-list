729b5b9158c08a7b46f45095e03f748430ea1633
Vagner S <vagnervst17@gmail.com>
2019-04-14 00:28:40 -0300

    Initial commit from Create React App

---
+++ src/containers/App/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import style from './style.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className={style.app}>
+        Games List
+      </div>
+    );
+  }
+}
+
+export default App;
